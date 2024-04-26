20820e929b17393812dab7f70cb7e50ab1a8c69c
daoauth <57783762+daoauth@users.noreply.github.com>
2024-04-27 00:32:24 +0900

    Update WormholeProvider.tsx

--- packages/wormhole-kit/src/components/WormholeProvider.tsx
+++ packages/wormhole-kit/src/components/WormholeProvider.tsx
@@ -1,13 +1,72 @@
-import { createContext } from 'react';
+import { createContext, useEffect, useRef, useState } from 'react';
 import type { ReactNode } from 'react';
 
+import {
+  type Chain,
+  type Network,
+  wormhole,
+  type Wormhole,
+} from '@wormhole-foundation/sdk';
+
+import { buildRedeemTx } from './buildRedeemTx';
+import { buildTransferTx } from './buildTransferTx';
+import { getPlatform } from './utils/getPlatform';
+
+import type { IReqRedeemTx, IReqTransferTx } from './types';
+
 export const WormholeContext = createContext({
-  // TODO
+  buildTransferTx: async (req: IReqTransferTx): Promise<string> => {
+    throw new Error();
+  },
+  buildRedeemTx: async (req: IReqRedeemTx): Promise<string> => {
+    throw new Error();
+  },
 });
 
-export const WormholeProvider = ({ children }: { children: ReactNode }) => {
+export const WormholeProvider = ({
+  chains,
+  network,
+  children,
+}: {
+  chains: Chain[];
+  network: Network;
+  children: ReactNode;
+}) => {
+  const initialized = useRef<boolean>(false);
+  const [wh, setWh] = useState<
+    Wormhole<'Mainnet' | 'Testnet' | 'Devnet'> | undefined
+  >(undefined);
+  useEffect(() => {
+    const initWh = async () => {
+      if (!initialized.current) {
+        initialized.current = true;
+        const temp = await wormhole(
+          network,
+          chains.map((chain) => getPlatform(chain)),
+        );
+        setWh(temp);
+      }
+    };
+    initWh();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
-    <WormholeContext.Provider value={{}}>
+    <WormholeContext.Provider
+      value={{
+        buildTransferTx: (req: IReqTransferTx) => {
+          if (wh) {
+            return buildTransferTx(wh, req);
+          }
+          throw new Error('wormhole error');
+        },
+        buildRedeemTx: (req: IReqRedeemTx) => {
+          if (wh) {
+            return buildRedeemTx(wh, req);
+          }
+          throw new Error('wormhole error');
+        },
+      }}
+    >
       {children}
     </WormholeContext.Provider>
   );
