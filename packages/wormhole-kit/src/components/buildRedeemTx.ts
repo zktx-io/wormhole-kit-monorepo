522f19f08fd1c0922787095a629fb04f6fa27a4b
daoauth <57783762+daoauth@users.noreply.github.com>
2024-04-27 01:24:30 +0900

    Update buildRedeemTx.ts

--- packages/wormhole-kit/src/components/buildRedeemTx.ts
+++ packages/wormhole-kit/src/components/buildRedeemTx.ts
@@ -1,3 +1,6 @@
+import { serializeTx } from './serializeTx';
+import { getUniversalAddress } from './utils/getUniversalAddress';
+
 import type { IReqRedeemTx } from './types';
 import type { Wormhole } from '@wormhole-foundation/sdk/dist/cjs';
 
@@ -5,5 +8,15 @@ export const buildRedeemTx = async (
   wh: Wormhole<'Mainnet' | 'Testnet' | 'Devnet'>,
   req: IReqRedeemTx,
 ): Promise<string> => {
-  throw new Error();
+  try {
+    const ctx = wh.getChain(req.source);
+    const [whm] = await ctx.parseTransaction(req.txHash);
+    const vaa = await wh.getVaa(whm!, 'TokenBridge:Transfer', 60_000);
+    const rcv = wh.getChain(req.receiver.chain);
+    const rcvTb = await rcv.getTokenBridge();
+    const redeem = rcvTb.redeem(getUniversalAddress(req.receiver), vaa!);
+    return serializeTx(req.receiver.chain, req.receiver.address, redeem);
+  } catch (error) {
+    throw new Error(`${error}`);
+  }
 };
