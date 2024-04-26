d2db220e9eb3c9ebcf6791a096bc471f265ab374
daoauth <57783762+daoauth@users.noreply.github.com>
2024-04-26 17:11:59 +0900

    sui example init

---
+++ packages/example-sui/src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          @zktx.io/wormhole-kit
+        </h1>
+        <h2>
+          Sui Example
+        </h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
