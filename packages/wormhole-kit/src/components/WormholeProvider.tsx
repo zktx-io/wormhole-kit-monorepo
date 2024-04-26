f7775a8627f7f9e8b1c72ff3868d08f98f675dfe
daoauth <57783762+daoauth@users.noreply.github.com>
2024-04-26 22:53:44 +0900

    remove vite

--- packages/wormhole-kit/src/components/WormholeProvider.tsx
+++ packages/wormhole-kit/src/components/WormholeProvider.tsx
@@ -1,15 +1,14 @@
-import React, { createContext } from 'react';
+import { createContext } from 'react';
 import type { ReactNode } from 'react';
 
 export const WormholeContext = createContext({
-    // TODO
+  // TODO
 });
-  
+
 export const WormholeProvider = ({ children }: { children: ReactNode }) => {
-    return (
-        <WormholeContext.Provider value={{}}>
-            sdafjksdjfl;kj
-            {children}
-        </WormholeContext.Provider>
-    );
+  return (
+    <WormholeContext.Provider value={{}}>
+      {children}
+    </WormholeContext.Provider>
+  );
 };
