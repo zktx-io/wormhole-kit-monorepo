4dc9b5bff5a3e110cf3598623f4147c028753237
daoauth <57783762+daoauth@users.noreply.github.com>
2024-04-26 17:26:54 +0900

    lint

--- packages/example-sui/src/App.tsx
+++ packages/example-sui/src/App.tsx
@@ -5,12 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>
-          @zktx.io/wormhole-kit
-        </h1>
-        <h2>
-          Sui Example
-        </h2>
+        <h1>@zktx.io/wormhole-kit</h1>
+        <h2>Sui Example</h2>
       </header>
     </div>
   );
