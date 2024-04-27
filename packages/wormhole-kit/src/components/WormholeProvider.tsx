f01469f7282596c28de48174359978c07795a98c
daoauth <57783762+daoauth@users.noreply.github.com>
2024-04-28 02:04:30 +0900

    Create WormholeProvider.tsx

---
+++ packages/wormhole-kit/src/components/WormholeProvider.tsx
@@ -0,0 +1,34 @@
+// eslint-disable-next-line import/default
+import React, { createContext } from 'react';
+
+import type { IReqRedeemTx, IReqTransferTx } from '../types';
+
+export const WormholeContext = createContext({
+  buildTransferTx: async (req: IReqTransferTx): Promise<string> => {
+    throw new Error();
+  },
+  buildRedeemTx: async (req: IReqRedeemTx): Promise<string> => {
+    throw new Error();
+  },
+});
+
+export const WormholeProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  return (
+    <WormholeContext.Provider
+      value={{
+        buildTransferTx: (req: IReqTransferTx) => {
+          throw new Error('wormhole error');
+        },
+        buildRedeemTx: (req: IReqRedeemTx) => {
+          throw new Error('wormhole error');
+        },
+      }}
+    >
+      {children}
+    </WormholeContext.Provider>
+  );
+};
