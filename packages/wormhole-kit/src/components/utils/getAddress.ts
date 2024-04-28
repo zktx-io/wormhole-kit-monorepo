67e6f33f0ceb766bbbb0d2531a25155a989c9f46
daoauth <57783762+daoauth@users.noreply.github.com>
2024-04-28 23:59:30 +0900

    Create getAddress.ts

---
+++ packages/wormhole-kit/src/components/utils/getAddress.ts
@@ -0,0 +1,30 @@
+import { AlgorandAddress } from '@wormhole-foundation/sdk-algorand';
+import { AptosAddress } from '@wormhole-foundation/sdk-aptos';
+import { EvmAddress } from '@wormhole-foundation/sdk-evm';
+import { SolanaAddress } from '@wormhole-foundation/sdk-solana';
+import { SuiAddress } from '@wormhole-foundation/sdk-sui';
+
+import type { IWhChain } from '../../types';
+
+export const getAddress = (
+  chain: IWhChain,
+  address: string,
+): AlgorandAddress | AptosAddress | EvmAddress | SolanaAddress | SuiAddress => {
+  switch (chain) {
+    case 'Algorand':
+      return new AlgorandAddress(address);
+    case 'Aptos':
+      return new AptosAddress(address);
+    case 'Solana':
+      return new SolanaAddress(address);
+    case 'Sui':
+      return new SuiAddress(address);
+    case 'Celo':
+    case 'Ethereum':
+    case 'Klaytn':
+      return new EvmAddress(address);
+    default:
+      break;
+  }
+  throw new Error();
+};
