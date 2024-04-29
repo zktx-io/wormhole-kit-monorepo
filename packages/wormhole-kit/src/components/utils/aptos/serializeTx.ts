2750c13c827c7d4d0865419190fb728654932705
daoauth <57783762+daoauth@users.noreply.github.com>
2024-04-29 16:39:55 +0900

    Create serializeTx.ts

---
+++ packages/wormhole-kit/src/components/utils/aptos/serializeTx.ts
@@ -0,0 +1,22 @@
+import type { UnsignedTransaction } from '@wormhole-foundation/sdk-connect';
+
+export const serializeTx = async (
+  sender: string,
+  txs: AsyncGenerator<UnsignedTransaction>,
+): Promise<string> => {
+  try {
+    const unsignedTx: string[] = [];
+    for await (const tx of txs) {
+      Buffer.from(tx.transaction.rawTransaction.bcsToBytes()).toString(
+        'base64',
+      );
+      unsignedTx.push(tx.transaction.serialize());
+    }
+    if (unsignedTx.length === 1) {
+      return unsignedTx[0];
+    }
+    throw new Error('multi transactions are not support');
+  } catch (error) {
+    throw new Error(`${error}`);
+  }
+};
