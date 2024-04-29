6c0c2b8e63988c7ed05bd2dc049f1a2379173004
daoauth <57783762+daoauth@users.noreply.github.com>
2024-04-29 11:06:49 +0900

    Create getNativeDecimals.ts

---
+++ packages/wormhole-kit/src/components/utils/getNativeDecimals.ts
@@ -0,0 +1,23 @@
+import { decimals } from '@wormhole-foundation/sdk-connect';
+
+import type { Chain } from '@wormhole-foundation/sdk-connect';
+
+export const getNativeDecimals = (chain: Chain): number => {
+  switch (chain) {
+    case 'Algorand':
+    case 'Aptos':
+    case 'Near':
+    case 'Btc':
+    case 'Solana':
+    case 'Sui':
+      return decimals.nativeDecimals(chain);
+    case 'Ethereum':
+    case 'Celo':
+    case 'Klaytn':
+      // TODO
+      return decimals.nativeDecimals('Evm');
+    default:
+      break;
+  }
+  throw new Error();
+};
