265924b3c19f2d67fb529d041cea8ef0fb5da8e5
daoauth <57783762+daoauth@users.noreply.github.com>
2024-05-01 21:39:56 +0900

    Update ChainIcon.tsx

--- packages/wormhole-kit/src/component/ChainIcon.tsx
+++ packages/wormhole-kit/src/component/ChainIcon.tsx
@@ -1,31 +1,31 @@
-import { Acala } from './icons/acala'
-import { Aptos } from './icons/aptos'
-import { Arbitrum } from './icons/arbitrum'
-import { Base } from './icons/base'
-import { Bsc } from './icons/bsc'
-import { Eth } from './icons/eth'
-import { Injective } from './icons/injective'
-import { Moonbeam } from './icons/moonbeam'
-import { Optimism } from './icons/optimism'
-import { Solana } from './icons/solana'
-import { Sui } from './icons/sui'
+import { Acala } from './icons/acala';
+import { Aptos } from './icons/aptos';
+import { Arbitrum } from './icons/arbitrum';
+import { Base } from './icons/base';
+import { Bsc } from './icons/bsc';
+import { Eth } from './icons/eth';
+import { Injective } from './icons/injective';
+import { Moonbeam } from './icons/moonbeam';
+import { Optimism } from './icons/optimism';
+import { Solana } from './icons/solana';
+import { Sui } from './icons/sui';
 
-import type { Chain } from "@wormhole-foundation/sdk-connect";
+import type { Chain } from '@wormhole-foundation/sdk-connect';
 
-export const ChainIcon = ({ chain }: { chain: Chain}) => {
-return (
+export const ChainIcon = ({ chain }: { chain: Chain }) => {
+  return (
     <>
-    { chain === 'Acala' && <Acala />}
-    { chain === 'Aptos' && <Aptos />}
-    { chain === 'Arbitrum' && <Arbitrum />}
-    { chain === 'Base' && <Base />}
-    { chain === 'Bsc' && <Bsc />}
-    { chain === 'Ethereum' && <Eth />}
-    { chain === 'Injective' && <Injective />}
-    { chain === 'Moonbeam' && <Moonbeam />}
-    { chain === 'Optimism' && <Optimism />}
-    { chain === 'Solana' && <Solana />}
-    { chain === 'Sui' && <Sui />}
+      {chain === 'Acala' && <Acala />}
+      {chain === 'Aptos' && <Aptos />}
+      {chain === 'Arbitrum' && <Arbitrum />}
+      {chain === 'Base' && <Base />}
+      {chain === 'Bsc' && <Bsc />}
+      {chain === 'Ethereum' && <Eth />}
+      {chain === 'Injective' && <Injective />}
+      {chain === 'Moonbeam' && <Moonbeam />}
+      {chain === 'Optimism' && <Optimism />}
+      {chain === 'Solana' && <Solana />}
+      {chain === 'Sui' && <Sui />}
     </>
-)
-}
+  );
+};
