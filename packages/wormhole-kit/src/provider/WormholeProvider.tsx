37d88910a730c912d60935210c4b028f5770fe78
daoauth <57783762+daoauth@users.noreply.github.com>
2024-05-01 12:36:04 +0900

    Update WormholeProvider.tsx

--- packages/wormhole-kit/src/provider/WormholeProvider.tsx
+++ packages/wormhole-kit/src/provider/WormholeProvider.tsx
@@ -1,6 +1,8 @@
 import type { ReactNode } from 'react';
 import { createContext, useContext, useEffect, useRef, useState } from 'react';
 
+import { Theme } from '@radix-ui/themes';
+
 import { buildRedeemTx } from './context/buildRedeemTx';
 import { buildTransferTx } from './context/buildTransferTx';
 import { loadPlotforms } from './loader';
@@ -8,7 +10,12 @@ import { loadPlotforms } from './loader';
 import type { IReqRedeemTx, IReqTransferTx, IWhPlatform } from './types';
 import type { Chain, Network } from '@wormhole-foundation/sdk-connect';
 
+import '@radix-ui/themes/styles.css';
+
 export const WormholeContext = createContext({
+  supportChains: (): Chain[] => {
+    throw new Error();
+  },
   buildTransferTx: async (req: IReqTransferTx): Promise<string> => {
     throw new Error();
   },
@@ -44,6 +51,9 @@ export const WormholeProvider = ({
   return (
     <WormholeContext.Provider
       value={{
+        supportChains: () => {
+          return chains;
+        },
         buildTransferTx: async (req: IReqTransferTx): Promise<string> => {
           return buildTransferTx(platforms, req);
         },
@@ -52,7 +62,7 @@ export const WormholeProvider = ({
         },
       }}
     >
-      {children}
+      <Theme>{children}</Theme>
     </WormholeContext.Provider>
   );
 };
