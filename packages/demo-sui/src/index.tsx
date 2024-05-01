1dda974052c2be7d3f9d8a5a8fbc723aeead5830
daoauth <57783762+daoauth@users.noreply.github.com>
2024-05-01 18:27:08 +0900

    Update index.tsx

--- packages/demo-sui/src/index.tsx
+++ packages/demo-sui/src/index.tsx
@@ -17,7 +17,7 @@ const queryClient = new QueryClient();
 const root = createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <StrictMode>
-    <WormholeProvider network="Testnet" chains={['Aptos', 'Sui']}>
+    <WormholeProvider network="Testnet" chains={['Aptos', 'Sui']} theme="dark">
       <QueryClientProvider client={queryClient}>
         <SuiClientProvider
           defaultNetwork="testnet"
