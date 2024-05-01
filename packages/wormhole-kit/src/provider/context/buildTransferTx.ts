ca0061da4cb572bf5a90865b794028b6f7267e50
daoauth <57783762+daoauth@users.noreply.github.com>
2024-05-01 13:51:31 +0900

    Update buildTransferTx.ts

--- packages/wormhole-kit/src/provider/context/buildTransferTx.ts
+++ packages/wormhole-kit/src/provider/context/buildTransferTx.ts
@@ -12,21 +12,29 @@ export const buildTransferTx = async (
   req: IReqTransferTx,
 ): Promise<string> => {
   try {
-    const sndTb = await getTokenBridge(req.sender.chain, platforms);
-    const txs = sndTb.transfer(
-      getUniversalAddress(req.sender),
-      {
-        chain: req.receiver.chain,
-        address: getUniversalAddress(req.receiver),
-      },
-      req.token.info ? (req.token.info.type as any) : 'native',
-      amount.units(
-        req.token.info
-          ? amount.parse(req.token.amount, req.token.info.decimals)
-          : amount.parse(req.token.amount, getNativeDecimals(req.sender.chain)),
-      ),
+    if (req.sender.chain !== req.receiver.chain) {
+      const sndTb = await getTokenBridge(req.sender.chain, platforms);
+      const txs = sndTb.transfer(
+        getUniversalAddress(req.sender),
+        {
+          chain: req.receiver.chain,
+          address: getUniversalAddress(req.receiver),
+        },
+        req.token.info ? (req.token.info.type as any) : 'native',
+        amount.units(
+          req.token.info
+            ? amount.parse(req.token.amount, req.token.info.decimals)
+            : amount.parse(
+                req.token.amount,
+                getNativeDecimals(req.sender.chain),
+              ),
+        ),
+      );
+      return serializeTx(req.sender.chain, req.sender.address, txs);
+    }
+    throw new Error(
+      `buildTransferTx : Source and Target chains must be different.`,
     );
-    return serializeTx(req.sender.chain, req.sender.address, txs);
   } catch (error) {
     throw new Error(`buildTransferTx : ${error}`);
   }
