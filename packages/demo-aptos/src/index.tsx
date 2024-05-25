ad37793edc64a7e34cca48408d74df0f286e090f
daoauth <57783762+daoauth@users.noreply.github.com>
2024-05-25 23:48:02 +0900

    Update index.tsx

--- packages/demo-aptos/src/index.tsx
+++ packages/demo-aptos/src/index.tsx
@@ -15,7 +15,7 @@ root.render(
   <StrictMode>
     <WhProvider
       network="Testnet"
-      chains={['Sui', 'Solana', 'Aptos', 'Celo', 'Polygon']}
+      chains={['Sui', 'Sepolia', 'Solana', 'Aptos', 'Celo', 'Polygon']}
       config={{
         chains: {
           Ethereum: {
